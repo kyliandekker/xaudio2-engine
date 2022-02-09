5b8ae5ab918533be3faecb4c7726fd253e0373c7
TriggerWinkle <kyliandekker2000@gmail.com>
2022-02-09 20:56:50 +0100

    Add panning effect

--- Audio Engine/src/Audio/Effects.cpp
+++ Audio Engine/src/Audio/Effects.cpp
@@ -25,5 +25,34 @@ unsigned char* effects::change_volume(unsigned char* data, uint32_t size, float 
 
 unsigned char* effects::change_panning(unsigned char* data, uint32_t size, float amount)
 {
-	return data;
+	float left = 1.0f, right = 1.0f;
+	if (amount < 0)
+	{
+		amount = abs(amount);
+		left -= amount;
+		right += amount;
+	}
+	else if (amount > 0)
+	{
+		amount = abs(amount);
+		right -= amount;
+		left += amount;
+	}
+	if (right > 1)
+		right = 1;
+	if (left > 1)
+		left = 1;
+	if (right < 0)
+		right = 0;
+	if (left < 0)
+		left = 0;
+
+	int16_t* array_16 = reinterpret_cast<int16_t*>(data);
+	for (uint32_t i = 0; i < size; i += 2)
+	{
+		array_16[i] = change_byte_volume(array_16[i], right);
+		array_16[i + 1] = change_byte_volume(array_16[i + 1], left);
+	}
+
+	return reinterpret_cast<unsigned char*>(array_16);
 }
