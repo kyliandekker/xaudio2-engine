cf767e3b8d2b18ff1326b4bf9b1f98eea7828a3a
TriggerWinkle <kyliandekker2000@gmail.com>
2022-02-27 01:22:41 +0100

    Added stereo to mono function

--- Audio Engine/src/wav/WavConverter.cpp
+++ Audio Engine/src/wav/WavConverter.cpp
@@ -62,5 +62,57 @@ namespace wav
 			}
 			return array_16;
 		}
+
+		unsigned char* ConvertMonoToStereo(unsigned char* a_Data, uint32_t& a_Size, uint16_t a_BlockAlign)
+		{
+			// Double the size.
+			a_Size = a_Size * 2;
+
+			unsigned char* array_16 = new unsigned char[a_Size];
+
+			int newIndex = 0;
+
+			for (int i = 0; i <= (a_Size / 2) - a_BlockAlign; i += a_BlockAlign)
+			{
+				int echo = 0;
+				while (echo < 2)
+				{
+					for (int j = 0; j < a_BlockAlign; j++)
+					{
+						array_16[newIndex] = static_cast<uint16_t>(a_Data[i + j]);
+						newIndex++;
+					}
+					echo++;
+				}
+			}
+
+			return array_16;
+		}
+
+		unsigned char* ConvertStereoToMono(unsigned char* a_Data, uint32_t& a_Size, uint16_t a_BlockAlign)
+		{
+			// Double the size.
+			a_Size = a_Size / 2;
+
+			unsigned char* array_16 = new unsigned char[a_Size];
+
+			int newIndex = 0;
+
+			for (int i = 0; i <= (a_Size * 2) - a_BlockAlign; i += a_BlockAlign)
+			{
+				int echo = 0;
+				while (echo < 2)
+				{
+					for (int j = 0; j < a_BlockAlign / 2; j++)
+					{
+						array_16[newIndex] = static_cast<uint16_t>(a_Data[i + j]);
+						newIndex++;
+					}
+					echo++;
+				}
+			}
+
+			return array_16;
+		}
 	}
 }
